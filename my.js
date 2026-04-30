7cf6d7e392cc07dd42300e4eae4bcffdc7b43775
Shivam Kumar <sk9400749@gmail.com>
2026-04-30 10:01:09 +0530

    branch ayush add

--- my.js
+++ my.js
@@ -1,1 +1,23 @@
-let myName="John Doe";
+let myName = "John Doe";
+
+function add(   a, b) {
+    return a + b;
+}           
+function subtract(a, b) {
+    return a - b;
+}   
+
+add(5, 3); // returns 8
+subtract(5, 3); // returns 2
+
+
+function multiply(a, b) {
+    return a * b;
+}   
+function division(a, b) {
+    return a / b;
+}
+
+console.log(multiply(5, 3)); // returns 15
+console.log(division(5, 3)); // returns 1
+
