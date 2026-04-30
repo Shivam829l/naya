ad04a2508f7ac2f62a2d36607d58c1bd96dd7afa
Shivam Kumar <sk9400749@gmail.com>
2026-04-30 09:45:38 +0530

    changes by shivam

--- my.js
+++ my.js
@@ -1,1 +1,9 @@
-let myName="John Doe";
+let myName = "John Doe";
+
+let aayush = {
+    age:25,
+    city:"New York",
+    profession:"Software Engineer"
+
+}
+console.log(aayush.profession + aayush.age);
